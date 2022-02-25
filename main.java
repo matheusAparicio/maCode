f78d652fdcb30461b54e295a352abbd52166a953
Matheus <matheusilva-m@outlook.com>
2022-02-24 23:41:27 -0300

    #005 choiseDifficulty method

--- main.java
+++ main.java
@@ -5,26 +5,35 @@ import java.util.Scanner;
 class Main {
 
   public static void main(String[] args) {
-    chooseLanguage();
+    Languages languages = new Languages();
+    String cL = chooseLanguage(languages); // cL = choosedLanguage
+    chooseDifficulty();
   }
 
-  static void chooseLanguage() {
+  static void chooseDifficulty() {
     // Definitions.
-    Languages languages = new Languages();
+
+    // Update.
+  }
+
+  static String chooseLanguage(Languages languages) {
+    // Definitions.
     Scanner scanner = new Scanner(System.in);
+    String cL = "0";
     String certain = "n";
 
     // Update.
     while (!isCertain(certain)) {
       System.out.println("Choose your language.\n1: Português\n2: English");
       System.out.print("(1/2): ");
-      String cL = scanner.nextLine(); // cL = choosedLanguage
+      cL = scanner.nextLine();
 
       System.out.println(languages.getChoosedLanguage(cL));
       System.out.print(languages.getYesNo(cL));
       certain = scanner.nextLine();
     }
     scanner.close();
+    return cL;
   }
 
   static Boolean isCertain(String certain) {
