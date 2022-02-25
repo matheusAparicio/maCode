6dc042b68685668e0a6e78360972a390f15d8c3c
Matheus <matheusilva-m@outlook.com>
2022-02-24 21:07:38 -0300

    #003 language choice loop

--- main.java
+++ main.java
@@ -1,23 +1,34 @@
 package maCode;
+
 import java.util.Scanner;
 
-
 class Main {
-    public static void main(String[] args) {
 
-      // Definitions.
-      Languages languages = new Languages();
-      Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
 
-      // Update.
+    // Definitions.
+    Languages languages = new Languages();
+    Scanner scanner = new Scanner(System.in);
+    String certain = "n";
+
+    // Update.
+    while (!isCertain(certain)) {
       System.out.println("Choose your language.\n1: Português\n2: English");
-      System.out.print("1 or 2: ");
+      System.out.print("(1/2): ");
       String cL = scanner.nextLine(); // cL = choosedLanguage
 
       System.out.println(languages.getChoosedLanguage(cL));
-      System.out.println(languages.getYesNo(cL));
-      String certain = scanner.nextLine();
-
-      scanner.close();
+      System.out.print(languages.getYesNo(cL));
+      certain = scanner.nextLine();
     }
+    scanner.close();
   }
+
+  static Boolean isCertain(String certain) {
+    if (certain.toLowerCase().equals("s") || certain.toLowerCase().equals("y")) {
+      return true;
+    }
+    return false;
+  }
+
+}
